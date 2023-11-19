1174f1214959fd436f6992e5f3f39d0d6c7829d5
Eduard <fedotovel@alrosa.ru>
2023-11-19 18:10:10 +0300

    Решение 2-го модуля

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,10 @@
 function sumSalary(salaries) {
-  // ваш код...
+  //  суммирование зарплаты с проверкой  
+  let sum = 0;
+  for (let key in salaries) {
+    if (isFinite (salaries[key]) && typeof salaries[key] == 'number') {
+      sum += salaries[key];
+    } 
+  }
+return (sum)  
 }
