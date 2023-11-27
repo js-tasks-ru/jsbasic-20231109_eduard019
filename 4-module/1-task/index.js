d4022f54a21fb9477acafa37394a9e3e74611dda
Eduard <fedotovel@alrosa.ru>
2023-11-27 13:32:35 +0300

    Решение 4-го модуля

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,14 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  // преобразование массива друзей в стандартный HTML список
+  const ul = document.createElement('ul');
+
+    friends.forEach(friend => {
+        const li = document.createElement('li');
+        li.textContent = `${friend.firstName} ${friend.lastName}`;
+        ul.appendChild(li);
+    });
+
+    return ul;
 }
+    
+
--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,33 @@
 function highlight(table) {
-  // ваш код...
+  // выделение ячеек/строк стилем по условиям
+  const rows = table.getElementsByTagName('tr');
+
+  for (let i = 1; i < rows.length; i++) {
+    const cells = rows[i].getElementsByTagName('td');
+    for (let j = 0; j < cells.length; j++) {
+        const cell = cells[j];
+
+        if (cell.getAttribute('data-available') === 'true') {
+            rows[i].classList.add('available');
+        } else if (cell.getAttribute('data-available') === 'false') {
+            rows[i].classList.add('unavailable');
+        }
+
+        if (cell.innerText === 'm') {
+            rows[i].classList.add('male');
+        } else if (cell.innerText === 'f') {
+            rows[i].classList.add('female');
+        }
+
+        if (j === 1 && parseInt(cell.innerText, 10) < 18) {
+            rows[i].style.textDecoration = 'line-through';
+        } 
+    } 
+
+    if (!cells[3].hasAttribute('data-available')) {
+        rows[i].setAttribute('hidden', true);
+    }
+ }
 }
+
+
